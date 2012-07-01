5aea81c6e149584a7258ede94675557b8f67cd36
Ryan Huang <ryan.deadbeef@gmail.com>
2012-06-30 22:23:50 -0700

    add llvm env setup file

---
+++ analyzer/llvm_env.sh
@@ -0,0 +1,5 @@
+export CC="clang -flto"
+export CFLAGS="-g3 -O0 -v"
+export CXXFLAGS="-g3 -O0 -v"
+export RANLIB=/bin/true
+export ARFLAGS="cru --plugin /home/ryan/Projects/llvm/build/lib/LLVMgold.so"
