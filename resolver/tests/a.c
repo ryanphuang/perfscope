7b1c1ba2b8423dd2c33aaefaf8e99780545b9e87
Ryan Huang <ryan.deadbeef@gmail.com>
2012-07-03 11:46:25 -0700

    add test case for matching function

---
+++ resolver/tests/a.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+static int id = 2;
+
+int foo(int c)
+{
+    c++;
+    if (c > 10) 
+{
+        c *= c;
+        return c;
+}
+    else {
+        id++;
+        if (id > 10) {
+            c++;
+        }
+        else {
+            c--;
+        }
+    }
+    int x = c * id;
+    x *= x;
+    return x;
+}
+
+int bar()
+{
+    int i;
+    int sum = 0;
+    for (i = 1; i < 10; i++) {
+       sum += i; 
+    }
+    return sum;
+}
+
+int main()
+{
+    bar(foo());
+}
