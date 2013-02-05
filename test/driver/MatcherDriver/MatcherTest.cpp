c07dfc26fb48c96155e05024edb73924a12785ef
Ryan Huang <ryan.deadbeef@gmail.com>
2013-02-04 16:07:25 -0800

    remove decoder test in matcher so it can be a unit test

--- test/driver/MatcherDriver/MatcherTest.cpp
+++ test/driver/MatcherDriver/MatcherTest.cpp
@@ -51,8 +51,6 @@ static cl::opt<int>  StripLen("strips",
 
 namespace {
 
-  static bool DEBUG = false;
-
   class MatcherTest: public ModulePass {
 
     public:
@@ -74,6 +72,8 @@ namespace {
         Function * f;
         int s = 0;
         errs() << "[" << begin << "," << end << "] touch {";
+        // do not specify the CU name so that it will match 
+        // the first CU
         Matcher::sp_iterator I = matcher.initMatch("");
         while ((f = matcher.matchFunction(I, scope)) != NULL ) {
           s++;
@@ -109,113 +109,6 @@ namespace {
         matcher.processLoops(li);
       }
 
-      void processDomTree(Function *F)
-      {
-        /** Getting Dominator Tree **/
-        DominatorTree & DT = getAnalysis<DominatorTree>(*F);
-        DT.print(errs()); // DFS print
-      }
-
-
-      void testAnalyzer(Module &M)
-      {
-
-        matcher.process(M);
-        matcher.setstrips(0, 7); // Set the strips of path in the debug info
-
-        PatchDecoder * decoder = new PatchDecoder(ClScopeListFile);
-        assert(decoder);
-        Patch *patch = NULL;
-        Chapter *chap = NULL;
-        Hunk * hunk = NULL;
-        Scope ls;
-        while((patch = decoder->next_patch()) != NULL) {
-          if (DEBUG)
-            errs() << "patch: " << patch->patchname << "\n";
-          while((chap = patch->next_chapter()) != NULL) {
-            if (DEBUG)
-              errs() << "chapter: " << chap->filename << "\n";
-            while((hunk = chap->next_hunk()) != NULL) {
-              if (DEBUG) {
-                errs() << "hunk: " << hunk->start_line << "\n";
-                errs() << hunk->ctrlseq << "\n";
-              }
-              assert(hunk->reduce());
-              //match(chap->fullname, matcher, hunk->enclosing_scope);
-              Function * f;
-              int s = 0;
-              Matcher::sp_iterator I = matcher.initMatch(chap->fullname);
-              errs() << "[" << hunk->enclosing_scope << "] might touch ";
-              Scope tmps = hunk->enclosing_scope; // match function will modify scope.
-              Hunk::iterator HI = hunk->begin(), HE = hunk->end();
-              while ((f = matcher.matchFunction(I, tmps)) != NULL ) {
-
-                // The enclosing scope is a rough estimation:
-                // We need to rely on the actual modification
-
-                // Hunk: [  (..M1..)       (..M2..)  (..M3..) ]
-                //                   {f1}
-
-                // Skip the modifications didn't reach function's beginning
-                while(HI != HE && (*HI)->scope.end < I->linenumber)
-                  HI++;
-
-                // no need to test the loop scope
-                if (HI == HE || (*HI)->scope.begin > I->lastline)
-                  continue;
-
-                s++;
-                const char *dname = cpp_demangle(I->name.c_str());
-                errs() << "scope #" << s << ": " << dname;
-                errs()  << " |=> " << hunk->enclosing_scope << "\n";
-                LoopInfo &li = getAnalysis<LoopInfo>(*f);
-                errs() << "\t";
-                if (li.begin() == li.end()) {
-                  errs() << "loop: none" << "\n";
-                  continue;
-                }
-
-                // Only look into overlapping modifications when
-                // there's loop inside this function.
-
-                //TODO more elegant
-                //TODO loop finder no need to restart
-                Loop * loop = NULL;
-                while(HI != HE && (*HI)->scope.begin <= I->lastline) {
-                  // Will only match the *fist* in top level
-                  // and innermost nested matching loop.
-                  // FIXME may need to get all the loops.
-                  loop = Matcher::matchLoop(li, (*HI)->scope);
-                  if (loop != NULL) {
-                    ScopeInfoFinder::getLoopScope(ls, loop);
-                    errs() << "loop: " << ls;
-                  }
-                  HI++;
-                }
-                if (loop == NULL)
-                  errs() << "loop: none" << "\n";
-                else
-                  errs() << "\n";
-              }
-              if (s == 0) {
-                errs() << "insignificant scope";
-              }
-              errs() << "\n";
-              /**
-                for (Hunk::iterator I = hunk->begin(), E = hunk->end();
-                I != E; I++) {
-                mod = *I;
-                assert(mod);
-                if (DEBUG)
-                cout << "mod: " << *mod << endl;
-                }
-               **/
-            }
-          }
-        }
-
-      }
-
       virtual bool runOnModule(Module &M) 
       {
         testMatching(M);
@@ -224,8 +117,6 @@ namespace {
 
       virtual void getAnalysisUsage(AnalysisUsage &AU) const {
         AU.setPreservesAll();
-        //AU.addRequired<DominatorTree>();
-        AU.addRequired<LoopInfo>();
       }
       virtual void print(raw_ostream &O, const Module *M) const {
         O << "This is MatcherTest !\n";
