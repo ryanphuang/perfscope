236a84fac7a0492ba8053b29a231d419bf7c75a4
Ryan Huang <ryan.deadbeef@gmail.com>
2013-02-22 13:31:22 -0800

    add caller test cases

---
+++ test/cases/caller_loop.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int bar1(int c)
+{
+  if (c < 100)
+    c += 10;
+  else
+    c -= 10;
+  return c * c;
+}
+int bar2(int c)
+{
+  if (c < 10)
+    c++;
+  else
+    c--;
+  return c << 1;
+}
+int foo(int c)
+{
+  int i;
+  int sum = 0;
+  for (i = 0; i < 10; i++) {
+    sum += bar1(i + c);
+  }
+  sum = bar2(sum);
+  return sum;
+}
+int main()
+{
+  printf("%d\n", foo(10));
+}
